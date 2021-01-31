90deecf511c65745f036c12e7e96d0c9dd5c74e7
Rishav-mngo <rishavinmngo@gmail.com>
2021-01-31 15:55:29 +0530

    correcting type

--- 4_1.c
+++ 4_1.c
@@ -5,7 +5,7 @@ int main()
     int n;
     printf(" Enter the two digit no. =");
     scanf("%d", &n);
-    printf(" reverce digit =%d%d\n", n % 10, n / 10);
+    printf("Reverse digit =%d%d\n", n % 10, n / 10);
     return 0;
 }
 
